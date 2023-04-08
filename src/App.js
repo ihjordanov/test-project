031d4091b423731f67747f2847741d6c3dc34f08
Ilian Jordanov <info@ihjordanov.com>
2023-04-08 22:04:42 +0300

    Test Component 5

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import TestComponent from "./components/TestComponent";
 
 function App() {
-  return (
-    <div className="App">
-123
-        456
-    </div>
-  );
+    return (
+        <div className="App">
+            <h4>Component Title <span>*</span></h4>
+            <TestComponent content={'Test Content'}/>
+        </div>
+    );
 }
 
 export default App;
